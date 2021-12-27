b13cf7d10db543fd3bee43241a774aac2ed34a04
Dmitry Karachentsev <dmitry.karachentsev@parsable.com>
2021-12-27 19:43:52 +0300

    init

---
+++ src/test/java/ru/ifmo/shortener/linkshortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ifmo.shortener.linkshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
