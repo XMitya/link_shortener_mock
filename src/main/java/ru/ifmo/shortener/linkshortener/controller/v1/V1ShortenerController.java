f7ddc81b9072368c17450c3118626938d38e0abd
Dmitry Karachentsev <dmitry.karachentsev@parsable.com>
2021-12-28 17:58:09 +0300

    Use templates

--- src/main/java/ru/ifmo/shortener/linkshortener/controller/v1/V1ShortenerController.java
+++ src/main/java/ru/ifmo/shortener/linkshortener/controller/v1/V1ShortenerController.java
@@ -2,15 +2,14 @@ package ru.ifmo.shortener.linkshortener.controller.v1;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import ru.ifmo.shortener.linkshortener.model.LongLink;
 import ru.ifmo.shortener.linkshortener.model.ShortLink;
 import ru.ifmo.shortener.linkshortener.service.LinkShortenerService;
 
-import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -27,14 +26,10 @@ public class V1ShortenerController {
     }
 
     @GetMapping("l/{rnd}")
-    public ResponseEntity<Void> expand(@PathVariable String rnd) {
+    public ModelAndView expand(@PathVariable String rnd) {
         return linkShortenerService.expand(new ShortLink(rnd))
-                .map(longLink -> {
-                    final HttpHeaders headers = new HttpHeaders();
-                    headers.put("Location", List.of(longLink.getLongLink()));
-
-                    return new ResponseEntity<Void>(headers, HttpStatus.MOVED_PERMANENTLY);
-                }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+                .map(longLink -> new ModelAndView("redirect:" + longLink))
+                .orElseGet(() -> new ModelAndView("/404", Map.of("rnd", rnd), HttpStatus.NOT_FOUND));
     }
 
 }
